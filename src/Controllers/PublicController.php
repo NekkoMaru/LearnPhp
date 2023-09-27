92d8fc36354136362f1e29e0ac04d94de128d030
NekkoMaru <102283939+NekkoMaru@users.noreply.github.com>
2023-09-27 07:59:33 +0000

    1

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -2,5 +2,13 @@
 namespace App\Controllers;
 
 class PublicController {
+    public function index(){
+        $name = 'Kaspar';
+        view('index', compact('name'));
+        //view('index', ['name'=>$name]);
+    }
 
+    public function about(){
+        view('about');
+    }
 }
